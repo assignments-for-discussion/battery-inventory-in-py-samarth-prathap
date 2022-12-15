c9ec14fddb164fffa15b039e4c658588e08d3235
samarth-prathap <64980013+samarth-prathap@users.noreply.github.com>
2022-12-15 17:14:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 
 def count_batteries_by_usage(cycles):
-  #initilize 3 counter variables
-  low=0
-  medium=0
-  high=0
-       	#classifying them based on the given condiions of being low medium and high
-  for batcharge in cycles:
-    #ittirate through each value in the list
-    if (batcharge<310) :
-      low+=1
-    elif (batcharge >309) and (batcharge < 930):
-      medium+=1
-    else (batcharge >929):
-      high+=1
-      
-      
- #return the values based on the classification information   
-  return {
-    "lowCount": low,
-    "mediumCount": medium,
-    "highCount": high
+    # counters to keep track
+    lowCount=0
+    mediumCount=0
+    highCount=0
+    # loop the list
+    for cycle in cycles:
+    # condition to group them based of the cycles 
+        if(cycle<310):
+          lowCount+=1
+        elif(cycle>=310 and cycle<929):
+          mediumCount+=1
+        else:
+          highCount+=1
+  
+    # returning the count of batteries.
+    return {
+    "lowCount": lowCount,
+    "mediumCount": mediumCount,
+    "highCount": highCount
   }
+
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
@@ -29,24 +29,7 @@ def test_bucketing_by_number_of_cycles():
   assert(counts["mediumCount"] == 3)
   assert(counts["highCount"] == 1)
   print("Done counting :)")
+
     
-  #edge or boundary conditions
-  counts = count_batteries_by_usage([100, 309, 310, 929, 900, 930])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 2)
-  assert(counts["highCount"] == 2)
-  print("Done counting :)")
- # a custom function to take input from the user
-def take_inputs():
-  n=int(input("Enter the number of batteries you want check efficiency cycle "))
-  cycles=[]
-  print("Enter the cycles")
-  for cycle in range(0,n):
-    cycles.append(int(input()))
-  return cycles
-  
-  
-
-
-if __name__ == '__main__':
+if _name_ == '_main_':
   test_bucketing_by_number_of_cycles()
